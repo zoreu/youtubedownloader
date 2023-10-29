69cf7cf8572a5a42b4d32835fea7405aea33f2d7
Joel Silva <53149347+zoreu@users.noreply.github.com>
2023-10-29 16:07:02 -0300

    Update easyyoutubedownloader.user.js

--- easyyoutubedownloader.user.js
+++ easyyoutubedownloader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Easy Youtube Downloader
-// @version     1.0.2
-// @date        2022-05-14
+// @version     1.0.3
+// @date        2023-10-29
 // @description Download any video and music (audio) from Youtube.
 // @author      zoreu
 // @compatible chrome
@@ -14,56 +14,53 @@
 // @namespace https://raw.githubusercontent.com/zoreu/youtubedownloader/master/easyyoutubedownloader.user.js
 // ==/UserScript==
 
+(function() {
+    'use strict';
 
-var AKoiMain = {
-    oXHttpReq: null,
-    vid: null,
-    oldUrl: null,
-    DocOnLoad: function(o) {
-        try {
-            if (null != o && null != o.body && null != o.location && (AKoiMain.vid = AKoiMain.getVid(o), AKoiMain.vid)) {
-                o.querySelector("#meta #notification-preference-button").setAttribute("style", "flex-wrap: wrap;");
-                var t = o.querySelector("#notification-preference-button"),
-                    e = o.querySelector("#y2mateconverter"),
-                    n = AKoiMain.GetCommandButton();
-                null == e && (null != t ? t.parentNode.insertBefore(n, t) : (t = o.querySelector("#eow-title")).parentNode.insertBefore(n, t)), AKoiMain.oldUrl = o.location.href, AKoiMain.checkChangeVid()
-            }
-            return !0
-        } catch (o) {
-            console.log("Ошибка в функции Y2mate.DocOnLoad. ", o)
-        }
-    },
-    checkChangeVid: function() {
-        setTimeout(function() {
-            AKoiMain.oldUrl == window.location.href ? AKoiMain.checkChangeVid() : AKoiMain.WaitLoadDom(window.document)
-        }, 1e3)
-    },
-    WaitLoadDom: function(o) {
-        AKoiMain.vid = AKoiMain.getVid(o), AKoiMain.vid ? null != o.querySelector("#meta #notification-preference-button") ? AKoiMain.DocOnLoad(o) : setTimeout(function() {
-            AKoiMain.WaitLoadDom(o)
-        }, 1e3) : AKoiMain.checkChangeVid()
-    },
-    goToY2mate: function(o) {
-        try {
-            var t = "https://y2mate.com/youtube/" + AKoiMain.vid + "/?utm_source=chrome_addon";
-            window.open(t, "_blank")
-        } catch (o) {
-            console.log("Ошибка в функции Y2mate.OnButtonClick. ", o)
-        }
-    },
-    GetCommandButton: function() {
-        try {
-            var o = document.createElement("button");
-            return o.id = "y2mateconverter", o.className = "yt-uix-tooltip", o.setAttribute("type", "button"), o.setAttribute("title", "Download by @ysEnma in Telegram"), o.innerHTML = "Download", o.addEventListener("click", function(o) {
-                AKoiMain.goToY2mate(o)
-            }, !0), o.setAttribute("style", "min-height:25px; position:relative; cursor: pointer; font: 13px Arial; background: #FC0A0A; color: #fff; text-transform: uppercase; display: block; padding: 10px 16px; margin:7px 7px; border: 1px solid #FC0A0A; border-radius: 2px; font-weight:bold"), o.setAttribute("onmouseover", "this.style.backgroundColor='#FC0A0A'"), o.setAttribute("onmouseout", "this.style.backgroundColor='#FC0A0A'"), o
-        } catch (o) {
-            console.log("Ошибка в функции Y2mate.GetCommandButton. ", o)
-        }
-    },
-    getVid: function(o) {
-        var t = o.location.toString().match(/^.*((m\.)?youtu\.be\/|vi?\/|u\/\w\/|embed\/|\?vi?=|\&vi?=)([^#\&\?]*).*/);
-        return !(!t || !t[3]) && t[3]
+    function getVideoID() {
+        const urlParams = new URLSearchParams(window.location.search);
+        return urlParams.get('v');
     }
-};
-AKoiMain.WaitLoadDom(window.document);
+
+    function generateRandomID() {
+        // Gere um ID aleatório único
+        return 'download-button-' + Math.random().toString(36).substr(2, 9);
+    }
+
+    function createDownloadButton() {
+        const ownerDiv = document.getElementById('owner');
+        if (ownerDiv) {
+            const videoID = getVideoID();
+            if (videoID) {
+                // Gere um ID aleatório para o link de download
+                const downloadLinkID = generateRandomID();
+
+                // Crie o link de download com o ID aleatório
+                const downloadLink = document.createElement('a');
+                downloadLink.id = downloadLinkID;
+                downloadLink.href = `https://www.y2mate.com/youtube/${videoID}`;
+                downloadLink.target = '_blank';
+
+                // Estilize o link de download
+                downloadLink.style.backgroundColor = 'red';
+                downloadLink.style.borderRadius = '10px';
+                downloadLink.style.width = '150px'; // Largura aumentada (altere conforme desejado)
+                downloadLink.style.height = '34px'; // Altura aumentada (altere conforme desejado)
+                downloadLink.style.textAlign = 'center'; // Centraliza o texto
+                downloadLink.style.lineHeight = '34px'; // Centraliza verticalmente
+                downloadLink.style.color = 'white'; // Define a cor do texto como branca
+                downloadLink.style.fontSize = '16px'; // Aumenta o tamanho do texto
+                downloadLink.style.textDecoration = 'none'; // Remove o sublinhado
+
+                // Defina o texto do link como "Download"
+                downloadLink.innerText = 'Download';
+
+                // Adicione o link de download à div com o ID "owner"
+                ownerDiv.appendChild(downloadLink);
+            }
+        }
+    }
+
+    // Adicione o botão após o carregamento completo da página
+    window.addEventListener('load', createDownloadButton);
+})();
