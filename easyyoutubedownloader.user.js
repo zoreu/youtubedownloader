52bc09717504208f335b149fafe595938a37b2b6
Joel Silva <53149347+zoreu@users.noreply.github.com>
2023-10-29 18:26:26 -0300

    Update easyyoutubedownloader.user.js

--- easyyoutubedownloader.user.js
+++ easyyoutubedownloader.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Easy Youtube Downloader
-// @version     1.0.3
+// @version     1.0.5
 // @date        2023-10-29
 // @description Download any video and music (audio) from Youtube.
 // @author      zoreu
@@ -8,59 +8,51 @@
 // @compatible firefox
 // @compatible opera
 // @compatible safari
+// @compatible edge
+// @compatible brave
 // @license         CC BY-SA
 // @license         https://creativecommons.org/licenses/by-sa/4.0
 // @match          *://*.youtube.com/*
 // @namespace https://raw.githubusercontent.com/zoreu/youtubedownloader/master/easyyoutubedownloader.user.js
+// @require https://code.jquery.com/jquery-2.2.4.min.js
+// @run-at document-end
 // ==/UserScript==
 
-(function() {
-    'use strict';
-
-    function getVideoID() {
-        const urlParams = new URLSearchParams(window.location.search);
-        return urlParams.get('v');
-    }
-
-    function generateRandomID() {
-        // Gere um ID aleatório único
-        return 'download-button-' + Math.random().toString(36).substr(2, 9);
-    }
-
-    function createDownloadButton() {
-        const ownerDiv = document.getElementById('owner');
-        if (ownerDiv) {
-            const videoID = getVideoID();
-            if (videoID) {
-                // Gere um ID aleatório para o link de download
-                const downloadLinkID = generateRandomID();
-
-                // Crie o link de download com o ID aleatório
-                const downloadLink = document.createElement('a');
-                downloadLink.id = downloadLinkID;
-                downloadLink.href = `https://www.y2mate.com/youtube/${videoID}`;
-                downloadLink.target = '_blank';
-
-                // Estilize o link de download
-                downloadLink.style.backgroundColor = 'red';
-                downloadLink.style.borderRadius = '10px';
-                downloadLink.style.width = '150px'; // Largura aumentada (altere conforme desejado)
-                downloadLink.style.height = '34px'; // Altura aumentada (altere conforme desejado)
-                downloadLink.style.textAlign = 'center'; // Centraliza o texto
-                downloadLink.style.lineHeight = '34px'; // Centraliza verticalmente
-                downloadLink.style.color = 'white'; // Define a cor do texto como branca
-                downloadLink.style.fontSize = '16px'; // Aumenta o tamanho do texto
-                downloadLink.style.textDecoration = 'none'; // Remove o sublinhado
-
-                // Defina o texto do link como "Download"
-                downloadLink.innerText = 'Download';
-
-                // Adicione o link de download à div com o ID "owner"
-                ownerDiv.appendChild(downloadLink);
+if ("undefined" == typeof(punisherYT)) {
+   var punisherYT = {
+      currentLink: '//www.y2mate.com/youtube',
+      currentMedia: null,
+      init: function() {
+         punisherYT.pageLoad();
+      },
+      addClick: function(document) {
+         if (document.URL.match('youtube.com') && new RegExp('v=[a-zA-Z0-9-_]{11}').exec(document.URL)) {
+            var tubeID = RegExp.lastMatch.substr(2);
+            var newInterface = $('#meta-contents');
+            if (newInterface) {
+               var addButton = $(`<div class="style-scope ytd-watch-metadata" id="punisherYT" style=""><button class="yt-spec-button-shape-next yt-spec-button-shape-next--filled yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-m" type="button" id="dropdownMenuButton" aria-haspopup="true" aria-expanded="false" style="padding: 10px; margin: 10px;"><a class="style-scope ytd-subscribe-button-renderer text-white" style="text-decoration: none; color: red; padding-left: 3px; padding-right: 3px" target="_blank" href="`+ punisherYT.currentLink + `/` + tubeID +`"><i class="fas fa-download"></i>Download</a></div>`);
+               var subsBtn = document.querySelector("#subscribe-button")
+               subsBtn.parentNode.insertBefore(addButton[0], subsBtn)
             }
-        }
-    }
-
-    // Adicione o botão após o carregamento completo da página
-    window.addEventListener('load', createDownloadButton);
-})();
+         }
+      },
+      pageLoad: function() {
+         if (document.body && document.domain == 'www.youtube.com') {
+            setInterval(punisherYT.inspectPg, 1000);
+            punisherYT.inspectPg();
+         }
+      },
+      inspectPg: function() {
+         if (punisherYT.currentMedia != document.URL && typeof ytplayer != 'undefined' && ytplayer) {
+            punisherYT.currentMedia = document.URL;
+            if ($('#punisherYT')) {
+               $('#punisherYT').remove()
+            }
+         }
+         if ($("#meta-contents")[0] && !$('#punisherYT')[0] && typeof ytplayer != 'undefined' && ytplayer) {
+            punisherYT.addClick(document);
+         }
+      },
+   };
+}
+punisherYT.init();
